112ab774030df09b0649fb5ab8a56625efde0eac
Runjia <runjia.chen44@gmail.com>
2014-10-20 12:19:30 -0700

    Starfield Program

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] number;
 
 
-void setup()
+public void setup()
 {
 	size(500,500);
 	number=new Particle[1000];
@@ -11,7 +27,7 @@ void setup()
 	}
 }
 
-void draw()
+public void draw()
 {
 	background(0);
 	for (int i=0;i<number.length;i++){
@@ -92,3 +108,12 @@ class OddballParticle implements Particle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
